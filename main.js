806b3db50691afcfa4d8b8ff881187aed29d0045
celoazevedo <meazevedo86@gmail.com>
2020-04-25 12:02:56 -0700

    working on styling

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
+// let submitButton = document.getElementById();
+// //  GET ELEMENT BY ID
+// var headerTitle = document.getElementById('main-header');
+// headerTitle.style.borderBottom = 'solid 3px #000';
+
+// //GET ELEMEBT BY CLASS NAME
+// var listItems = document.getElementsByClassName('word-list');
+// listItems[2].textContent = 'hellow';
+
+// //GET ELEMENT BY TAG NAME
+// var li = document.getElementsByTagName('li');
+
 //do not use String and Array methods
 //isPalindrome() must take in a string and return true or false
 //declare a new array, can pop and unshift, push shift... into new array
 //no splice, split,join
-function isPalindrome(word){
-    let len = word.length;
-    for (let i = 0; i < len / 2; i++){
-        if (word[i] !== word[len - 1 - i]){
-            return false;
-        } else {
-            return true;
-        }
-    }
-}
+// function isPalindrome(word){
+//     let len = word.length;
+//     for (let i = 0; i < len / 2; i++){
+//         if (word[i] !== word[len - 1 - i]){
+//             return false;
+//         } else {
+//             return true;
+//         }
+//     }
+// }
 // isPalindrome('racecar');
-console.log(isPalindrome('racecar'));
-console.log(isPalindrome('tacocat'));
-console.log(isPalindrome('anna'));  
-console.log(isPalindrome('hello !**! olleh'));
-console.log(isPalindrome('12raar  21'));
+// console.log(isPalindrome('racecar'));
+// console.log(isPalindrome('tacocat'));
+// console.log(isPalindrome('anna'));  
+// console.log(isPalindrome('hello !**! olleh'));
+// console.log(isPalindrome('12raar  21'));
 
-console.dir(document);
+// console.dir(document);
 
 
-let submitButton = document.getElementById();
+var button = document.getElementById('button').addEventListener('click', isPalindrome);
+
+// function isPalindrome(word){
+//     let len = word.length;
+//     for (let i = 0; i < len / 2; i++){
+//         if (word[i] !== word[len - 1 - i]){
+//             return false;
+//         } else {
+//             return true;
+//         }
+//     }
+//     console.log(isPalindrome('racecar'));    
+// }
+
+function isPalindrome(){
+    console.log("buttonClicked");
+
+}
